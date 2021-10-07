96a4341870ac7ccbda07e4ad71e79ab538c2eb4f
Marcin Kozubek <mkozubek@protonmail.com>
2021-10-07 17:38:06 +0200

    Some further refectoring

--- new.r
+++ new.r
@@ -11,6 +11,7 @@ accuracy <- c(2,2,2,2,2)
 # We need to handle vector of objects representing GA params like: maxiter, mutationType etc
 
 setClassUnion("DemeOrNULL", members = c("Deme", "NULL"))
+setClassUnion("numericOrNULL", members = c("numeric", "NULL"))
 
 setClass("Deme", slots = c(
   population = "matrix",
@@ -19,7 +20,8 @@ setClass("Deme", slots = c(
   best_solution = "numeric",
   parent = "DemeOrNULL",
   best_solutions_per_metaepoch = "numeric",
-  best_fitnesses_per_metaepoch = "numeric"
+  best_fitnesses_per_metaepoch = "numeric",
+  sprout = "numeric"
 ))
 
 rnorm_population <- function(mean, sd, lower, upper, population_size){
@@ -59,36 +61,42 @@ create_deme <- function(lower, upper, parent, population_size) {
                                    population_size = population_size)
   }
   new("Deme", 
-      population = population, 
-      level = new_deme_level,
-      parent = NULL)
+        population = population, 
+        level = new_deme_level,
+        sprout = ifelse(is.null(parent), NULL, parent@best_solution)
+        parent = parent)
+}
+
+
+max_euclidean_distance_sprouting_condition <- function(max_distance) {
+    function(potential_sprout, level_demes) {
+        single_deme_condition <- function (deme) {
+            if(is.null(deme@sprout)) FALSE
+            else euclidean_distance(deme@sprout, potential_parent) < max_distance
+        }
+        length(Filter(single_deme_condition, level_demes)) == 0
+    }
 }
 
 ga_metaepoch <- function(config_ga) {
   function(fitness, suggestions, lower, upper, tree_level) {
-    config <- config_ga[[tree_level]]
+    config <- config_ga[tree_level]
     legal_passed_param_names <- Filter(function(name) { name %in% formalArgs(ga) }, config)
-    params <- list()
+    params <- list("maxiter" = 10)
     for(param_name in legal_passed_param_names) { 
       params[param_name] <- passed_params[param_name]
     } 
-    GA <- ga(type = "real-valued", 
-             fitness = fitness, 
-             lower = lower,
-             upper = upper,
-             # population = gaControl(type)$population,
-             # selection = gaControl(type)$selection,
-             # crossover = gaControl(type)$crossover,
-             # mutation = gaControl(type)$mutation,
-             # pcrossover = 0.8,
-             # pmutation = 0.1,
-             suggestions = suggestions, 
-             popSize = length(suggestions),
-             maxiter = 10, # To powinno być parametryzowane
-             # run = 10, # To też powinno być parametryzowane
-             keepBest = TRUE,
-             monitor = FALSE)
-    list("parent" = GA@solution, "population" = GA@population, "parent_value" = GA@fitnessValue)
+    params$fitness = fitness
+    params$lower = lower
+    params$upper = upper
+    params$suggestions = suggestions
+    params$monitor = FALSE
+
+    # TODO Always better to have more data :)
+    params$keepBest = TRUE
+
+    GA <- do.call(GA::ga, params)
+    list("solution" = GA@solution, "population" = GA@population, "value" = GA@fitnessValue)
   }
 }
 
@@ -99,38 +107,42 @@ local_stopping_condition <- function(deme) {
   best_fitness_metaepoch < metaepoch_count - max_metaepochs_without_improvement
 }
 
-local_stopping_condition2 <- function(deme) {
-  max_metaepochs_without_improvement <- 5
-  last_five_best_fitness <- tail(deme@best_fitnesses_per_metaepoch, n=max_metaepochs_without_improvement)
-  all(sort(last_five_best_fitness) == last_five_best_fitness)
-}
-
-global_stopping_condition <- function(execution_time) {
+global_stopping_condition <- function(metaepochs,  execution_time) {
 
 }
 
-hms <- function(max_tree_height, f, lower, upper, sigma, accuracy, population_size){
+hms <- function(
+    max_tree_height = 5,
+    fitness,
+    lower,
+    upper,
+    sigma,
+    population_size,
+    run_metaepoch = ga_metaepoch(list(list(), list(), list(), list(), list())), # TODO :)
+    global_stopping_condition = global_stopping_condition,
+    local_stopping_condition = local_stopping_condition,
+    sprouting_condition = max_euclidean_distance_sprouting_condition(0.5),
+){
   root <- create_deme(lower, upper, NULL, population_size)
   active_demes <- c(root)
-  parents <- mapply(function(x) c(), 0:max_tree_height)
   best_solution <- -Inf
   best_fitness <- -Inf
-  for(i in 1:1000){
+  for(i in 1:1000) {
     new_demes <- c()
-    for(deme in active_demes){
-      metaepoch_result <- do_ga_metaepoch(fitness, deme@population, lower, upper)
+    for(deme in active_demes) {
+      metaepoch_result <- run_metaepoch(fitness, deme@population, lower, upper)
       ### UPDATE DEME:
-      potential_parent <- metaepoch_result$parent
-      current_best <- metaepoch_result$parent_value
+      potential_sprout <- metaepoch_result$solution
+      metaepoch_best <- metaepoch_result$value
       deme@population <- metaepoch_result$population
-      deme@best_fitnesses_per_metaepoch <- c(deme@best_fitnesses_per_metaepoch, current_best)
+      deme@best_fitnesses_per_metaepoch <- c(deme@best_fitnesses_per_metaepoch, metaepoch_best)
       deme@best_solutions_per_metaepoch <- c(deme@best_solutions_per_metaepoch, parent)
       deme_best <- ifelse(length(deme@best_fitness)==0), -Inf, deme@best_fitness)
-      if(current_best > deme_best) {
-        deme@best_fitness <- current_best
-        deme@best_solution <- potential_parent
+      if(metaepoch_best > deme_best) {
+        deme@best_fitness <- metaepoch_best
+        deme@best_solution <- potential_sprout
       }
-      new_demes <- c(new_demes, deme)
+      
       if(local_stopping_condition(deme)){
         if(deme@best_fitness > best_fitness) {
           best_fitness <- deme@best_fitness
@@ -138,13 +150,14 @@ hms <- function(max_tree_height, f, lower, upper, sigma, accuracy, population_si
         }
         next
       }
+      new_demes <- c(new_demes, deme)
+
       if(deme@level > max_tree_height) next
-      level_parents <- parents[[deme@level]]
-      sprouting_condition <- function (level_parent) {euclidean_distance(level_parent, potential_parent) < 0.5}
-      if(is.null(level_parents) || length(Filter(sprouting_condition, level_parents)) == 0){
+      
+      level_demes <- Filter(function(d) { d@level == deme@level }, active_demes)
+      if(sprouting_condition(potential_sprout, level_demes))
         new_deme <- create_deme(lower, upper, population_size, deme@level, initial_points=potential_parent)
         new_demes <- c(new_demes, new_deme)
-        parents[[deme@level]] <- c(parents[[deme@level]], list(potential_parent))
       }
     }   
     active_demes <- new_demes
