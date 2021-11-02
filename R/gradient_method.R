384b21cff1766ae63345d736269f82563b2897f5
WojtAcht <wachtelik@gmail.com>
2021-11-02 22:54:51 +0100

    Add gradient_method interface; Run gradient_method for leaves

--- R/gradient_method.R
+++ R/gradient_method.R
@@ -1,18 +1,25 @@
-run_gradient_method <- function(deme,
-                                fitness,
-                                optim_args = list(method = "L-BFGS-B",
-                                                  poptim = 0.05,
-                                                  pressel = 0.5,
-                                                  control = list(fnscale = -1, maxit = 100))){
-  opt <- suppressWarnings(
-    do.call(stats::optim,
-            c(list(fn = fitness,
-                   par = deme@best_solution,
-                   method = optim_args$method,
-                   lower = optim_args$lower,
-                   upper = optim_args$upper,
-                   control = optim_args$control)))
+#' Function that runs gradient method for one deme.
+#'
+#' @param deme - Deme
+#' @param fitness - function
+#' @param optim_args - list of additional parameters
+#'
+#' @export
+#'
+#' @examples
+default_run_gradient_method <- function(deme, fitness, optim_args) {
+  result <- suppressWarnings(
+    do.call(
+      stats::optim,
+      c(list(
+        fn = fitness,
+        par = deme@best_solution,
+        method = optim_args$method,
+        lower = optim_args$lower,
+        upper = optim_args$upper,
+        control = optim_args$control
+      ))
     )
-  result <- list("solution" = opt$par, "value" = opt$value, population = deme@population)
-  update_deme(result, deme)
+  )
+  list("solution" = result$par, "value" = result$value, "population" = deme@population)
 }
