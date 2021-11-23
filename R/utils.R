da92e382f0810172774320e437de43cafcc240ca
WojtAcht <wachtelik@gmail.com>
2021-11-23 21:39:17 +0100

    Export metrics

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,25 @@
+#' Title
+#'
+#' @param x - numeric
+#' @param y - numeric
+#'
+#' @return
+#' @export
+#'
+#' @examples
 euclidean_distance <- function(x, y) {
   sqrt(sum((x - y)^2))
 }
 
+#' Title
+#'
+#' @param x - numeric
+#' @param y - numeric
+#'
+#' @return
+#' @export
+#'
+#' @examples
 manhattan_distance <- function(x, y) {
   sum(mapply(abs, x - y))
 }
