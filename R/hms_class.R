385d30bc54d0aa9e6bdb2f96e25c63cbee14b364
WojtAcht <wachtelik@gmail.com>
2022-01-06 19:21:24 +0100

    Run check; Fix bad variable name and descriptions

--- R/hms_class.R
+++ R/hms_class.R
@@ -269,44 +269,43 @@ setMethod("printBlockedSprouts", "hms", function(object) {
     cat("\n")
   }
 })
-           
+
 #' Title
 #'
 #' @param object - hms s4 object
+#' @param dimensions - two selected dimensions
 #'
-#' @return
 #' @export
-#'
-#' @examples
 setGeneric("plotPopulation", function(object, dimensions) standardGeneric("plotPopulation"))
 
 #' Title
 #'
-#' @param hms - hms s4 object
+#' @param object - hms s4 object
+#' @param dimensions - two selected dimensions
 #'
-#' @return
 #' @export
-#'
-#' @examples
 setMethod("plotPopulation", "hms", function(object, dimensions) {
-  dimensions_names <- mapply(function(n) {paste("x",  toString(n), sep="")}, dimensions)
-  last_metaepoch_demes <- tail(object@metaepoch_snapshots, n=1)[[1]]@demes
+  dimensions_names <- mapply(function(n) {
+    paste("x", toString(n), sep = "")
+  }, dimensions)
+  last_metaepoch_demes <- utils::tail(object@metaepoch_snapshots, n = 1)[[1]]@demes
   demes_ids <- mapply(function(deme) deme@id, last_metaepoch_demes)
   populations_with_indices <- lapply(
     last_metaepoch_demes,
     function(deme) {
-      deme_population <- deme@population[,dimensions]
+      deme_population <- deme@population[, dimensions]
       colnames(deme_population) <- dimensions_names
       deme_df <- as.data.frame(deme_population)
       deme_df["color"] <- match(deme@id, demes_ids)
       deme_df
-    })
+    }
+  )
   population <- Reduce(rbind, populations_with_indices)
   plot(
-    x=population[,dimensions_names[[1]]],
-    y=population[,dimensions_names[[2]]],
-    col=population$color,
-    pch=16,
+    x = population[, dimensions_names[[1]]],
+    y = population[, dimensions_names[[2]]],
+    col = population$color,
+    pch = 16,
     xlab = dimensions_names[[1]],
     ylab = dimensions_names[[2]]
   )
@@ -315,51 +314,52 @@ setMethod("plotPopulation", "hms", function(object, dimensions) {
 #' Title
 #'
 #' @param object - hms s4 object
+#' @param path - path
+#' @param dimensions - two selected dimensions
 #'
-#' @return
 #' @export
-#'
-#' @examples
 setGeneric("saveMetaepochsPopulations", function(object, path, dimensions) standardGeneric("saveMetaepochsPopulations"))
 
 #' Title
 #'
-#' @param hms - hms s4 object
+#' @param object - hms s4 object
+#' @param path - path
+#' @param dimensions - two selected dimensions
 #'
-#' @return
 #' @export
-#'
-#' @examples
 setMethod("saveMetaepochsPopulations", "hms", function(object, path, dimensions) {
-  dimensions_names <- mapply(function(n) {paste("x",  toString(n), sep="")}, dimensions)
-  xlim <- c(result@lower[[dimensions[[1]]]], result@upper[[dimensions[[1]]]])
-  ylim <- c(result@lower[[dimensions[[2]]]], result@upper[[dimensions[[2]]]])
+  dimensions_names <- mapply(function(n) {
+    paste("x", toString(n), sep = "")
+  }, dimensions)
+  xlim <- c(object@lower[[dimensions[[1]]]], object@upper[[dimensions[[1]]]])
+  ylim <- c(object@lower[[dimensions[[2]]]], object@upper[[dimensions[[2]]]])
   demes_ids_per_snapshot <- mapply(
     function(s) mapply(function(d) d@id, s@demes),
-    result@metaepoch_snapshots
+    object@metaepoch_snapshots
   )
   demes_ids <- Reduce(c, demes_ids_per_snapshot)
-  for(i in seq_along(object@metaepoch_snapshots)) {
+  for (i in seq_along(object@metaepoch_snapshots)) {
     snapshot <- object@metaepoch_snapshots[[i]]
     snapshot_demes <- snapshot@demes
     populations_with_indices <- lapply(
       snapshot_demes,
       function(deme) {
-        deme_population <- deme@population[,dimensions]
+        deme_population <- deme@population[, dimensions]
         colnames(deme_population) <- dimensions_names
         deme_df <- as.data.frame(deme_population)
         deme_df["color"] <- match(deme@id, demes_ids)
         deme_df
-    })
+      }
+    )
     snapshot_population <- Reduce(rbind, populations_with_indices)
-    jpeg(
-      file = paste(path, "/snapshot", toString(i), ".png", sep=""),
+    grDevices::jpeg(
+      file = paste(path, "/snapshot", toString(i), ".png", sep = ""),
       width = 480,
       height = 480
     )
     plot(
-      x = snapshot_population[,dimensions_names[[1]]],
-      y = snapshot_population[,dimensions_names[[2]]],
+      x = snapshot_population[, dimensions_names[[1]]],
+      y = snapshot_population[, dimensions_names[[2]]],
       col = snapshot_population$color,
       pch = 16,
       xlab = dimensions_names[[1]],
@@ -368,6 +368,6 @@ setMethod("saveMetaepochsPopulations", "hms", function(object, path, dimensions)
       ylim = ylim,
       main = paste("Snapshot", toString(i))
     )
-    dev.off()
+    grDevices::dev.off()
   }
 })
