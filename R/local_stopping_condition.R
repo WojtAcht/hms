8fa025906ef32cbdb408780f2cd163ad7e7dd5f7
Marcin Kozubek <mkozubek@protonmail.com>
2022-01-09 17:22:45 +0100

    Add local stopping condition stopping a deme after given number of metaepochs without an active child

--- R/local_stopping_condition.R
+++ R/local_stopping_condition.R
@@ -18,7 +18,7 @@ lsc_metaepochs_without_improvement <- function(max_metaepochs_without_improvemen
   }
 }
 
-#' Factory function for a local stopping consition that stops a deme
+#' Factory function for a local stopping condition that stops a deme
 #' after given number of fitness function evaluations has been made
 #' in that deme.
 #'
@@ -33,6 +33,31 @@ lsc_metaepochs_without_improvement <- function(max_metaepochs_without_improvemen
 lsc_max_fitness_evaluations <- function(max_evaluations) {
   function(deme, previous_metaepoch_snapshots) {
     !is_root(deme) & deme@evaluations_count > max_evaluations
+  }
+}
+
+#' Factory function for a local stopping condition that stops a deme
+#' after given number of metaepochs have past since last metaepoch during
+#' which this deme had an active child.
+#'
+#' @param metaepochs_limit - number of metaepochs that a deme can be active
+#' without any active child
+#'
+#' @return Function that can be used as a local stopping condition for hms.
+#'
+#' @export
+#'
+#' @example
+#' local_stopping_condition <- lsc_metaepochs_without_active_child(3)
+lsc_metaepochs_without_active_child <- function(metaepochs_limit) {
+  function(deme, previous_metaepoch_snapshots) {
+    has_active_child <- function(snapshot) {
+      any(mapply(function(d) {
+        d@parent_id == deme@id && d@is_active
+      }, snapshot@demes))
+    }
+    any(has_active_child,
+        tail(previous_metaepoch_snapshots, n = metaepochs_limit))
   }
 }
 
